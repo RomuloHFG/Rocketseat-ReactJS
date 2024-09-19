487934899845a9b53ef309e636de1d81c9f2ac17
Rômulo Henrique Ferreira Gonçalves <romulo.henrique.dev@gmail.com>
2024-09-19 10:41:15 -0300

    "ReactJS - Rocketseat"

--- Aula 1/01 - Fundamentos-Reactjs/src/components/Comment.jsx
+++ Aula 1/01 - Fundamentos-Reactjs/src/components/Comment.jsx
@@ -7,23 +7,25 @@ export default function Comment() {
         <div className={styles.comment}>
             <img src="https://github.com/RomuloHFG.png" alt="" />
             <div className={styles.commentBox}>
-                <div className={styles.commentContent}></div>
+                <div className={styles.commentContent}>
                 <header>
                     <div className={styles.authorAndTime}>
                         <strong>Rômulo Henrique</strong>
                         <time title='17 de setembro às 17:03' dateTime="2024-09-17 17:03:40">Cerca de 1h atrás</time>
                     </div>
                     <button title="Deletar comentário">
-                        <Trash size={20} />
+                        <Trash size={24} />
                     </button>
                 </header>
                 <p>Muito bom devon, parabens</p>
+                </div>
                 <footer>
                     <button>
                         <ThumbsUp />
                         Aplaudir <span>20</span>
                     </button>
                 </footer>
+                
             </div>
         </div>
     )
