3b9821a92e0bb62fcabc5d62d24ba34d371b441d
Rômulo Henrique Ferreira Gonçalves <romulo.henrique.dev@gmail.com>
2024-09-20 12:36:27 -0300

    ReactJS - Rocketseat

--- Aula 1/01 - Fundamentos-Reactjs/src/components/Post.jsx
+++ Aula 1/01 - Fundamentos-Reactjs/src/components/Post.jsx
@@ -3,11 +3,24 @@ import Comment from './Comment';
 import Avatar from './Avatar';
 import ptBR from 'date-fns/locale/pt-BR';
 import { format, formatDistanceToNow } from 'date-fns';
+import { useState } from 'react';
 
 export default function Post({ author, publishedAt, content }) {
+
+    const [comments, setComments] = useState([
+        1,
+        2,
+    ])
+
     const publishedDateFormatted = format(publishedAt, " d 'de' LLLL 'às' HH:mm'h'", { locale: ptBR })
 
     const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, { locale: ptBR, addSuffix: 'true' })
+
+    function handleCreateNewComment(){
+        event.preventDefault()
+
+       setComments([1,2,3]);
+    }
 
     return (
         <article className={styles.post}>
@@ -35,20 +48,19 @@ export default function Post({ author, publishedAt, content }) {
                 })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
                 <textarea placeholder="Deixe um comentário" />
+
                 <footer>
                     <button type='submit'>Publicar</button>
                 </footer>
             </form>
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
-
-
+                {comments.map(comment=> {
+                    return <Comment/>;
+                })}
             </div>
         </article>
     )
