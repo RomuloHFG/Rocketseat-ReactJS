c2d01f81b6ffd72315c9972b86e035e48f93f803
Rômulo Henrique Ferreira Gonçalves <romulo.henrique.dev@gmail.com>
2024-09-23 12:52:16 -0300

    ReactJS - Rocketseat

--- Aula 1/01 - Fundamentos-Reactjs/src/components/Comment.jsx
+++ Aula 1/01 - Fundamentos-Reactjs/src/components/Comment.jsx
@@ -1,40 +1,50 @@
 import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 import Avatar from "./Avatar";
+import { useState } from "react";
 
 
 
-export default function Comment({content, onDeleteComment}) {
-    
-    function handleDeleteComment () {
-    
+export default function Comment({ content, onDeleteComment }) {
+
+    const [likeCount, setLikeCount] = useState(0)
+
+    function handleDeleteComment() {
+
         onDeleteComment(content);
     }
-    
-    
+
+    function handleLikeComment (){
+        setLikeCount(likeCount + 1);
+    }
+
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/RomuloHFG.png" alt="" />
+
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
-                <header>
-                    <div className={styles.authorAndTime}>
-                        <strong>Rômulo Henrique</strong>
-                        <time title='17 de setembro às 17:03' dateTime="2024-09-17 17:03:40"></time>
-                    </div>
-                    <button onClick={handleDeleteComment} title="Deletar comentário">
-                        <Trash size={24} />
-                    </button>
-                </header>
-                <p>{content}</p>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Rômulo Henrique</strong>
+                            <time title='17 de setembro às 17:03' dateTime="2024-09-17 17:03:40"></time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                  
+                    <p>{content}</p>
                 </div>
+               
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
-                
             </div>
         </div>
     )
