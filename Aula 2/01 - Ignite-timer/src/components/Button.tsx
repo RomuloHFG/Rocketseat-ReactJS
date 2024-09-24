b39807a66157a3f98a1c587357b1042c2469fc43
Rômulo Henrique Ferreira Gonçalves <romulo.henrique.dev@gmail.com>
2024-09-24 17:33:57 -0300

    ReactJs - Rocketseat

--- Aula 2/01 - Ignite-timer/src/components/Button.tsx
+++ Aula 2/01 - Ignite-timer/src/components/Button.tsx
@@ -1,9 +1,10 @@
-import styles from "./Button.module.css";
+import { ButtonContainer, ButtonVariant } from "./Button.styles";
+
 
 interface ButtonProps{
-    color?: "primary" | "secondary" | "danger" | "success";
+    variant?: ButtonVariant;
 }
 
-export default function Button({color = "primary"}: ButtonProps){
-return <button className={`${styles.button} ${styles[color]}`} >Enviar</button>
+export default function Button({ variant = "primary" }: ButtonProps){
+return <ButtonContainer variant={variant}>Enviar</ButtonContainer> 
 }
