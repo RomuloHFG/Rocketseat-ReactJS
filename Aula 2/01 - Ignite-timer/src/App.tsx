fffcef2e01ba6bb30671d0278c385c3b753f0ef5
Rômulo Henrique Ferreira Gonçalves <romulo.henrique.dev@gmail.com>
2024-09-24 17:49:25 -0300

    ReactJs - Rocketseat

--- Aula 2/01 - Ignite-timer/src/App.tsx
+++ Aula 2/01 - Ignite-timer/src/App.tsx
@@ -1,17 +1,18 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+
 import Button from "./components/Button";
 
 export default function App() {
 
   return (
-    <>
-      <Button variant="primary"/>
-      <Button variant="secondary"/>
-      <Button variant="success"/>
-      <Button variant="danger"/>
-      <Button/>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
 
-    </>
+    </ThemeProvider>
   )
 }
-
-
