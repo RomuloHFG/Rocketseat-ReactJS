cad51335e70a62b2cae4358ead38cd9c404e2c45
Rômulo Henrique Ferreira Gonçalves <romulo.henrique.dev@gmail.com>
2024-09-26 14:59:31 -0300

    ReactJs - Rocketseat

--- Aula 2/01 - Ignite-timer/src/pages/Home/Index.tsx
+++ Aula 2/01 - Ignite-timer/src/pages/Home/Index.tsx
@@ -1,16 +1,16 @@
 import { Play } from "phosphor-react";
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from './styles'
-import { useState } from "react";
-import {useForm} from "react-hook-form"
-import {zodResolver} from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
 import * as zod from "zod"
+import { useState } from "react";
 
 const newCycleFormValidationSchema = zod.object({
     task: zod.string().min(1, "Informe a tarefa"),
     minutesAmount: zod
-    .number()
-    .min(5, "O ciclo precisa ser de no mínimo 5 minutos")
-    .max(60, "O ciclo precisa ser de no máximo 60 minutos")
+        .number()
+        .min(5, "O ciclo precisa ser de no mínimo 5 minutos")
+        .max(60, "O ciclo precisa ser de no máximo 60 minutos")
 })
 
 
@@ -18,38 +18,65 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+    id: string;
+    task: string;
+    minutesAmount: number;
+}
+
 
 export default function Home() {
-    const {register, handleSubmit, watch, reset} = useForm<NewCycleFormData>({
-        resolver: zodResolver(newCycleFormValidationSchema),
-    defaultValues: {
-        task: '',
-        minutesAmount: 0,
-    }
-    })
-
+    const [cycles, setCycles] = useState<Cycle[]>([])
+    const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
     
 
-    function handleCreateNewCycle(data:NewCycleFormData) {
-        console.log(data)
-        reset();
-        
+
+
+    const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+        resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: '',
+            minutesAmount: 0,
+        }
+    })
+
+
+
+    function handleCreateNewCycle(data: NewCycleFormData) {
+        const id = String(new Date().getTime())
+
+        const newCycle: Cycle = {
+            id,
+            task: data.task,
+            minutesAmount: data.minutesAmount,
+        }
+
+        setCycles((state) => [...state, newCycle])
+        setActiveCycleId(id)
+
+
+        reset()
+
     }
+
+    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+    console.log(activeCycle)
 
     const task = watch('task')
     const isSubmitDisabled = !task
-    
+
     return (
         <HomeContainer>
             <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
                 <FormContainer>
                     <label htmlFor="task" >Vou trabalhar em</label>
-                    <TaskInput 
-                    id="task" 
-                    list="task-suggestions" 
-                    placeholder="Dê um nome para o seu projeto" 
-                    {...register('task')}
-                    
+                    <TaskInput
+                        id="task"
+                        list="task-suggestions"
+                        placeholder="Dê um nome para o seu projeto"
+                        {...register('task')}
+
                     />
 
                     <datalist id="task-suggestions">
@@ -60,14 +87,14 @@ export default function Home() {
                     </datalist>
 
                     <label htmlFor="minutesAmount">durante</label>
-                    <MinutesAmountInput 
-                    type="number" 
-                    id="minutesAmount" 
-                    placeholder="00" 
-                    step={5}
-                    min={5}
-                    max={60}
-                    {...register('minutesAmount', {valueAsNumber: true})}
+                    <MinutesAmountInput
+                        type="number"
+                        id="minutesAmount"
+                        placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
+                        {...register('minutesAmount', { valueAsNumber: true })}
 
                     />
 
@@ -82,9 +109,9 @@ export default function Home() {
                     <span>0</span>
                 </CountdownContainer>
                 <StartCountdownButton disabled={isSubmitDisabled} type="submit">
-                    <Play size={24}/> 
-                    Começar 
-                    </StartCountdownButton>
+                    <Play size={24} />
+                    Começar
+                </StartCountdownButton>
 
             </form>
         </HomeContainer>
