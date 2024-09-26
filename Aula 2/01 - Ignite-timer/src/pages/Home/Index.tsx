77c564d47abfa2add449d932c8be02182ca15c53
Rômulo Henrique Ferreira Gonçalves <romulo.henrique.dev@gmail.com>
2024-09-26 16:02:29 -0300

    ReactJs - Rocketseat

--- Aula 2/01 - Ignite-timer/src/pages/Home/Index.tsx
+++ Aula 2/01 - Ignite-timer/src/pages/Home/Index.tsx
@@ -39,15 +39,20 @@ export default function Home() {
 
     const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
-    useEffect(() =>{
-        if(activeCycle){
-            setInterval(() => {
+    useEffect(() => {
+        let interval: number;
+
+        if (activeCycle) {
+            interval = setInterval(() => {
                 setAmountSecondsPassed(
                     differenceInSeconds(new Date(), activeCycle.startDate),
-            );
+                );
             }, 1000)
         }
-    },  [activeCycle])
+        return () => {
+            clearInterval(interval);
+        }
+    }, [activeCycle])
 
 
     function handleCreateNewCycle(data: NewCycleFormData) {
@@ -62,6 +67,7 @@ export default function Home() {
 
         setCycles((state) => [...state, newCycle])
         setActiveCycleId(id)
+        setAmountSecondsPassed(0)
 
 
         reset()
@@ -77,6 +83,12 @@ export default function Home() {
 
     const minutes = String(minutesAmount).padStart(2, "0")
     const seconds = String(secondsAmount).padStart(2, "0")
+
+    useEffect(() => {
+        if (activeCycle){
+            document.title = `${minutes}:${seconds}`
+        }
+    }, [minutes, seconds, activeCycle])
 
     const task = watch('task')
     const isSubmitDisabled = !task
