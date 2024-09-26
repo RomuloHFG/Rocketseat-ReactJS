d00c1e9ec8df00505fe58cde44eff17a04b686f0
Rômulo Henrique Ferreira Gonçalves <romulo.henrique.dev@gmail.com>
2024-09-26 16:54:41 -0300

    ReactJs - Rocketseat

--- Aula 2/01 - Ignite-timer/src/pages/Home/Index.tsx
+++ Aula 2/01 - Ignite-timer/src/pages/Home/Index.tsx
@@ -10,7 +10,7 @@ const newCycleFormValidationSchema = zod.object({
     task: zod.string().min(1, "Informe a tarefa"),
     minutesAmount: zod
         .number()
-        .min(5, "O ciclo precisa ser de no mínimo 5 minutos")
+        .min(1, "O ciclo precisa ser de no mínimo 5 minutos")
         .max(60, "O ciclo precisa ser de no máximo 60 minutos")
 })
 
@@ -154,7 +154,7 @@ export default function Home() {
                         id="minutesAmount"
                         placeholder="00"
                         step={5}
-                        min={5}
+                        min={1}
                         max={60}
                         disabled={!!activeCycle}
                         {...register('minutesAmount', { valueAsNumber: true })}
